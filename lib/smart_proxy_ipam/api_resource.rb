c171e27fc6708641d0c11fec329c64c0b6f173cb
lakedh <131279636+lakedh@users.noreply.github.com>
2025-03-12 17:11:58 +0100

    adding content type to delete too

--- lib/smart_proxy_ipam/api_resource.rb
+++ lib/smart_proxy_ipam/api_resource.rb
@@ -33,6 +33,7 @@ module Proxy::Ipam
       request = Net::HTTP::Delete.new(uri)
       request[@auth_header] = @token
       request['Accept'] = 'application/json'
+      request['Content-Type'] = 'application/json'
 
       Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
         http.request(request)
